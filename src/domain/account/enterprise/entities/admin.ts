513561bb7b01e575af27e971744da40926bdb98b
felipesanderp <felipe.panisson@hotmail.com>
2024-01-24 16:54:15 -0300

    cpf value object, admin and deliveryman entitys and config vitest tests

---
+++ src/domain/account/enterprise/entities/admin.ts
@@ -0,0 +1,28 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+interface AdminProps {
+  name: string
+  cpf: string
+  password: string
+}
+
+export class Admin extends Entity<AdminProps> {
+  get name() {
+    return this.props.name
+  }
+
+  get cpf() {
+    return this.props.cpf
+  }
+
+  get password() {
+    return this.props.password
+  }
+
+  static create(props: AdminProps, id?: UniqueEntityID) {
+    const admin = new Admin(props, id)
+
+    return admin
+  }
+}
---
+++ src/domain/account/enterprise/entities/deliveryman.ts
@@ -0,0 +1,32 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+interface DeliverymanProps {
+  name: string
+  cpf: string
+  password: string
+}
+
+export class Deliveryman extends Entity<DeliverymanProps> {
+  get name() {
+    return this.props.name
+  }
+
+  get cpf() {
+    return this.props.cpf
+  }
+
+  get password() {
+    return this.props.password
+  }
+
+  set password(password: string) {
+    this.props.password = password
+  }
+
+  static create(props: DeliverymanProps, id?: UniqueEntityID) {
+    const deliveryman = new Deliveryman(props, id)
+
+    return deliveryman
+  }
+}
