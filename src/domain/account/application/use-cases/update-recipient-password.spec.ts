82c7baaa9a460c66bab140e86b4a3f6fee89efce
felipesanderp <felipe.panisson@hotmail.com>
2024-01-26 10:38:53 -0300

    update recipient password use case and tests

---
+++ src/domain/account/application/use-cases/update-recipient-password.spec.ts
@@ -0,0 +1,69 @@
+import { FakeHasher } from 'test/cryptography/fake-hasher'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { SamePasswordError } from './errors/same-password-error'
+import { InMemoryRecipientsRepository } from 'test/repositories/in-memory-recipients.repository'
+import { UpdateRecipientPasswordUseCase } from './update-recipient-password'
+import { InMemoryRecipientAddressesRepository } from 'test/repositories/in-memory-recipient-address-repository'
+import { makeRecipient } from 'test/factories/make-recipient'
+
+let inMemoryRecipientsRepository: InMemoryRecipientsRepository
+let inMemoryRecipientAddressesRepository: InMemoryRecipientAddressesRepository
+let fakeHasher: FakeHasher
+
+let sut: UpdateRecipientPasswordUseCase
+
+describe('Update Deliveryman Password', () => {
+  beforeEach(() => {
+    inMemoryRecipientAddressesRepository =
+      new InMemoryRecipientAddressesRepository()
+    inMemoryRecipientsRepository = new InMemoryRecipientsRepository(
+      inMemoryRecipientAddressesRepository,
+    )
+    fakeHasher = new FakeHasher()
+
+    sut = new UpdateRecipientPasswordUseCase(
+      inMemoryRecipientsRepository,
+      fakeHasher,
+    )
+  })
+
+  it('should be able to update a recipient password', async () => {
+    const recipient = makeRecipient(
+      {
+        password: await fakeHasher.hash('123456'),
+      },
+      new UniqueEntityID('recipient-id'),
+    )
+    inMemoryRecipientsRepository.items.push(recipient)
+
+    const result = await sut.execute({
+      recipientId: 'recipient-id',
+      newPassword: '12345678910',
+    })
+
+    const hashedNewPassword = await fakeHasher.hash('12345678910')
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryRecipientsRepository.items[0].password).toEqual(
+      hashedNewPassword,
+    )
+  })
+
+  it('should not be able to update deliveryman password with same old password', async () => {
+    const recipient = makeRecipient(
+      {
+        password: await fakeHasher.hash('123456'),
+      },
+      new UniqueEntityID('recipient-id'),
+    )
+    inMemoryRecipientsRepository.items.push(recipient)
+
+    const result = await sut.execute({
+      recipientId: 'recipient-id',
+      newPassword: await fakeHasher.hash('123456'),
+    })
+
+    expect(result.isLeft()).toBe(true)
+    expect(result.value).toBeInstanceOf(SamePasswordError)
+  })
+})
---
+++ src/domain/account/application/use-cases/update-recipient-password.ts
@@ -0,0 +1,47 @@
+import { Either, left, right } from '@/core/either'
+import { HashGenerator } from '../cryptography/hash-generator'
+import { SamePasswordError } from './errors/same-password-error'
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
+import { RecipientsRepository } from '../repositories/recipients-repository'
+
+interface UpdateRecipientPasswordUseCaseRequest {
+  recipientId: string
+  newPassword: string
+}
+
+type UpdateRecipientPasswordUseCaseResponse = Either<
+  ResourceNotFoundError | SamePasswordError,
+  null
+>
+
+export class UpdateRecipientPasswordUseCase {
+  constructor(
+    private recipientsRepository: RecipientsRepository,
+    private hashGenerator: HashGenerator,
+  ) {}
+
+  async execute({
+    recipientId,
+    newPassword,
+  }: UpdateRecipientPasswordUseCaseRequest): Promise<UpdateRecipientPasswordUseCaseResponse> {
+    const recipient = await this.recipientsRepository.findById(recipientId)
+
+    if (!recipient) {
+      return left(new ResourceNotFoundError())
+    }
+
+    const isSamePassword = recipient.password === newPassword
+
+    if (isSamePassword) {
+      return left(new SamePasswordError())
+    }
+
+    const hashedPassword = await this.hashGenerator.hash(newPassword)
+
+    recipient.password = hashedPassword
+
+    await this.recipientsRepository.save(recipient)
+
+    return right(null)
+  }
+}
