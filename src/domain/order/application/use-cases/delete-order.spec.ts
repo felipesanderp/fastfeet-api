3d65738b8bf2d2c2ded4a526b998ded9906ba23d
felipesanderp <felipe.panisson@hotmail.com>
2024-02-02 14:08:09 -0300

    delete order use case test

---
+++ src/domain/order/application/use-cases/delete-order.spec.ts
@@ -0,0 +1,46 @@
+import { InMemoryCustomerAddressesRepository } from 'test/repositories/in-memory-customer-addresses-repository'
+import { InMemoryCustomersRepository } from 'test/repositories/in-memory-customers-repository'
+import { InMemoryImagesRepository } from 'test/repositories/in-memory-images-repository'
+import { InMemoryOrderImagesRepository } from 'test/repositories/in-memory-order-images-repository'
+import { InMemoryOrdersRepository } from 'test/repositories/in-memory-orders-repository'
+
+import { DeleteOrderUseCase } from './delete-order'
+import { makeOrder } from 'test/factories/make-order'
+
+let inMemoryCustomerAddressesRepository: InMemoryCustomerAddressesRepository
+let inMemoryCustomersRepository: InMemoryCustomersRepository
+let inMemoryOrderImagesRepository: InMemoryOrderImagesRepository
+let inMemoryImagesRepository: InMemoryImagesRepository
+let inMemoryOrdersRepository: InMemoryOrdersRepository
+
+let sut: DeleteOrderUseCase
+
+describe('Delete Order', () => {
+  beforeEach(() => {
+    inMemoryCustomerAddressesRepository =
+      new InMemoryCustomerAddressesRepository()
+    inMemoryCustomersRepository = new InMemoryCustomersRepository()
+    inMemoryOrderImagesRepository = new InMemoryOrderImagesRepository()
+    inMemoryImagesRepository = new InMemoryImagesRepository()
+    inMemoryOrdersRepository = new InMemoryOrdersRepository(
+      inMemoryOrderImagesRepository,
+      inMemoryImagesRepository,
+      inMemoryCustomersRepository,
+      inMemoryCustomerAddressesRepository,
+    )
+
+    sut = new DeleteOrderUseCase(inMemoryOrdersRepository)
+  })
+
+  it('should be able to delete a order', async () => {
+    const order = makeOrder()
+    inMemoryOrdersRepository.items.push(order)
+
+    const result = await sut.execute({
+      orderId: order.id.toString(),
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryOrdersRepository.items).toHaveLength(0)
+  })
+})
