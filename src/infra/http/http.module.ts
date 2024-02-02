00d62d5f240e6fc8d2eddc598c1c0033bbdafa4d
felipesanderp <felipe.panisson@hotmail.com>
2024-02-02 16:33:50 -0300

    storage module and cloudfare r2 config

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 
 import { DatabaseModule } from '../database/database.module'
 import { CryptographyModule } from '../cryptography/cryptography.module'
+import { StorageModule } from '../storage/store.module'
 
 @Module({
-  imports: [DatabaseModule, CryptographyModule],
+  imports: [DatabaseModule, CryptographyModule, StorageModule],
   controllers: [],
   providers: [],
 })
