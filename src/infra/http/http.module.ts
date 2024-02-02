9a970b561524e247457631662148fd8921ef01c7
felipesanderp <felipe.panisson@hotmail.com>
2024-02-02 16:35:29 -0300

    geolocation api service

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common'
 import { DatabaseModule } from '../database/database.module'
 import { CryptographyModule } from '../cryptography/cryptography.module'
 import { StorageModule } from '../storage/store.module'
+import { ServicesModule } from '../services/services.module'
 
 @Module({
-  imports: [DatabaseModule, CryptographyModule, StorageModule],
+  imports: [DatabaseModule, CryptographyModule, StorageModule, ServicesModule],
   controllers: [],
   providers: [],
 })
