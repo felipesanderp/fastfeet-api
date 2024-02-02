1227a9d5ffb7818a46d35468c68ed3416b3aa9c7
felipesanderp <felipe.panisson@hotmail.com>
2024-02-02 17:10:14 -0300

    prisma admin repo and mappers

---
+++ src/infra/database/prisma/repositories/prisma-admins-repository.ts
@@ -0,0 +1,69 @@
+import { AdminsRepository } from '@/domain/account/application/repositories/admins-repository'
+import { Admin } from '@/domain/account/enterprise/entities/admin'
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma.service'
+import { PrismaAdminMapper } from '../mappers/prisma-admin-mapper'
+
+@Injectable()
+export class PrismaAdminsRepository implements AdminsRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async findByCpf(cpf: string): Promise<Admin | null> {
+    const admin = await this.prisma.user.findUnique({
+      where: {
+        cpf,
+        role: 'ADMIN',
+      },
+    })
+
+    if (!admin) {
+      return null
+    }
+
+    return PrismaAdminMapper.toDomain(admin)
+  }
+
+  async findById(id: string): Promise<Admin> {
+    const admin = await this.prisma.user.findUnique({
+      where: {
+        id,
+        role: 'ADMIN',
+      },
+    })
+
+    if (!admin) {
+      return null
+    }
+
+    return PrismaAdminMapper.toDomain(admin)
+  }
+
+  async create(admin: Admin): Promise<void> {
+    const data = PrismaAdminMapper.toPrisma(admin)
+
+    await this.prisma.user.create({
+      data,
+    })
+  }
+
+  async save(admin: Admin): Promise<void> {
+    const data = PrismaAdminMapper.toPrisma(admin)
+
+    await this.prisma.user.update({
+      where: {
+        id: admin.id.toString(),
+      },
+      data,
+    })
+  }
+
+  async delete(admin: Admin): Promise<void> {
+    const data = PrismaAdminMapper.toPrisma(admin)
+
+    await this.prisma.user.delete({
+      where: {
+        id: data.id,
+      },
+    })
+  }
+}
