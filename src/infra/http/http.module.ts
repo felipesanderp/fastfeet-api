6e85caa9c3c37f9858ed965148ae0c1e03edc368
felipesanderp <felipe.panisson@hotmail.com>
2024-02-03 22:31:08 -0300

    register controller and provider for auth admin

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -5,9 +5,16 @@ import { CryptographyModule } from '../cryptography/cryptography.module'
 import { StorageModule } from '../storage/store.module'
 import { ServicesModule } from '../services/services.module'
 
+import { AuthenticateAdminController } from "./controllers/authenticate-admin.controller"
+import { AuthenticateAdminUseCase } from "@/domain/account/application/use-cases/authenticate-admin"
+
 @Module({
   imports: [DatabaseModule, CryptographyModule, StorageModule, ServicesModule],
-  controllers: [],
-  providers: [],
+  controllers: [
+    AuthenticateAdminController
+  ],
+  providers: [
+    AuthenticateAdminUseCase
+  ],
 })
 export class HttpModule {}
