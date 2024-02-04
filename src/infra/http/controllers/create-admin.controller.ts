9d35d6ca55d8b4857d61ebc3b69237ae5e6c1b98
felipesanderp <felipe.panisson@hotmail.com>
2024-02-04 12:25:35 -0300

    create admin controller and admin presenter to http

---
+++ src/infra/http/controllers/create-admin.controller.ts
@@ -0,0 +1,59 @@
+import { z } from 'zod'
+
+import {
+  BadRequestException,
+  Body,
+  ConflictException,
+  Controller,
+  HttpCode,
+  Post,
+} from '@nestjs/common'
+
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { CreateAdminUseCase } from '@/domain/account/application/use-cases/create-admin'
+import { AccountAlreadyExistsError } from '@/domain/account/application/use-cases/errors/account-already-exists-error'
+import { AdminPresenter } from '../presenters/admin-presenter'
+
+const createAdminBodySchema = z.object({
+  name: z.string(),
+  cpf: z.string(),
+  password: z.string(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(createAdminBodySchema)
+
+type CreateAdminBodySchema = z.infer<typeof createAdminBodySchema>
+
+@Controller('/accounts/admin')
+export class CreateAdminController {
+  constructor(private createAdmin: CreateAdminUseCase) {}
+
+  @Post()
+  @HttpCode(201)
+  async handle(@Body(bodyValidationPipe) body: CreateAdminBodySchema) {
+    const { name, cpf, password } = body
+
+    const result = await this.createAdmin.execute({
+      name,
+      cpf,
+      password,
+    })
+
+    if (result.isLeft()) {
+      const error = result.value
+
+      switch (error.constructor) {
+        case AccountAlreadyExistsError:
+          throw new ConflictException(error.message)
+        default:
+          throw new BadRequestException()
+      }
+    }
+
+    const { admin } = result.value
+
+    return {
+      admin: AdminPresenter.toHTTP(admin),
+    }
+  }
+}
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -5,14 +5,17 @@ import { CryptographyModule } from '../cryptography/cryptography.module'
 import { StorageModule } from '../storage/store.module'
 import { ServicesModule } from '../services/services.module'
 
-import { AuthenticateAdminController } from "./controllers/authenticate-admin.controller"
-import { AuthenticateAdminUseCase } from "@/domain/account/application/use-cases/authenticate-admin"
+import { AuthenticateAdminController } from './controllers/authenticate-admin.controller'
+import { AuthenticateAdminUseCase } from '@/domain/account/application/use-cases/authenticate-admin'
 
-import { AuthenticateDeliverymanController } from "./controllers/authenticate-deliveryman.controller"
-import { AuthenticateDeliverymanUseCase } from "@/domain/account/application/use-cases/authenticate-deliveryman"
+import { AuthenticateDeliverymanController } from './controllers/authenticate-deliveryman.controller'
+import { AuthenticateDeliverymanUseCase } from '@/domain/account/application/use-cases/authenticate-deliveryman'
 
-import { AuthenticateRecipientController } from "./controllers/authenticate-recipient.controller"
-import { AuthenticateRecipientUseCase } from "@/domain/account/application/use-cases/authenticate-recipient"
+import { AuthenticateRecipientController } from './controllers/authenticate-recipient.controller'
+import { AuthenticateRecipientUseCase } from '@/domain/account/application/use-cases/authenticate-recipient'
+
+import { CreateAdminController } from './controllers/create-admin.controller'
+import { CreateAdminUseCase } from '@/domain/account/application/use-cases/create-admin'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule, StorageModule, ServicesModule],
@@ -20,11 +23,13 @@ import { AuthenticateRecipientUseCase } from "@/domain/account/application/use-c
     AuthenticateAdminController,
     AuthenticateDeliverymanController,
     AuthenticateRecipientController,
+    CreateAdminController,
   ],
   providers: [
     AuthenticateAdminUseCase,
     AuthenticateDeliverymanUseCase,
     AuthenticateRecipientUseCase,
+    CreateAdminUseCase,
   ],
 })
 export class HttpModule {}
