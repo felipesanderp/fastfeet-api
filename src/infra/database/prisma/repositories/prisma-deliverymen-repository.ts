5d0fefc5fd4e442abe9eddc42170be54fdec5323
felipesanderp <felipe.panisson@hotmail.com>
2024-02-03 22:53:14 -0300

    deliverymen prisma mapper and repo

---
+++ src/infra/database/prisma/repositories/prisma-deliverymen-repository.ts
@@ -0,0 +1,74 @@
+import { DeliverymanRepository } from "@/domain/account/application/repositories/deliveryman-repository";
+import { Deliveryman } from "@/domain/account/enterprise/entities/deliveryman";
+import { PrismaService } from "../prisma.service";
+import { Injectable } from "@nestjs/common";
+import { PrismaDeliverymanMapper } from "../mappers/prisma-deliveryman-mapper";
+
+@Injectable()
+export class PrismaDeliverymenRepository implements DeliverymanRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async findAll(): Promise<Deliveryman[] | null> {
+    const deliverymen = await this.prisma.user.findMany({
+      where: {
+        role: 'DELIVERYMAN'
+      }
+    })
+
+    if (deliverymen.length <= 0) {
+      return null
+    }
+
+    return deliverymen.map(PrismaDeliverymanMapper.toDomain)
+  }
+
+  async findByCpf(cpf: string): Promise<Deliveryman | null> {
+    const deliveryman = await this.prisma.user.findUnique({
+      where: {
+        cpf,
+      }
+    })
+
+    if (!deliveryman) {
+      return null
+    }
+
+    return PrismaDeliverymanMapper.toDomain(deliveryman)
+  }
+
+  async findById(id: string): Promise<Deliveryman | null> {
+    const deliveryman = await this.prisma.user.findUnique({
+      where: {
+        id,
+      }
+    })
+
+    if (!deliveryman) {
+      return null
+    }
+
+    return PrismaDeliverymanMapper.toDomain(deliveryman)
+  }
+
+  async create(deliveryman: Deliveryman): Promise<void> {
+    const data = PrismaDeliverymanMapper.toPrisma(deliveryman)
+
+    await this.prisma.user.create({
+      data,
+    })
+  }
+
+  async save(deliveryman: Deliveryman): Promise<void> {
+    const data = PrismaDeliverymanMapper.toPrismaUpdate(deliveryman)
+
+    await this.prisma.user.update(data)
+  }
+
+  async delete(deliveryman: Deliveryman): Promise<void> {
+    await this.prisma.user.delete({
+      where: {
+        id: deliveryman.id.toString(),
+      }
+    })
+  }
+}
