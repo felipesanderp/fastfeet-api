e0763a4cb1077fe4c163b9fdcfd980111c7cb47c
felipesanderp <felipe.panisson@hotmail.com>
2024-02-09 08:11:22 -0300

    enable cors to dev origin

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -6,7 +6,11 @@ import { EnvService } from './env/env.service'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
-  app.enableCors()
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  })
   const configService = app.get(EnvService)
   const port = configService.get('PORT')
 
