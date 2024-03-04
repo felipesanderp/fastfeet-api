4492206b2881c89b7a0762a106aed769b42d3b58
felipesanderp <felipe.panisson@hotmail.com>
2024-03-04 20:14:33 -0300

    update recipient profile controller

---
+++ src/infra/http/controllers/update-recipient-profile.controller.ts
@@ -0,0 +1,77 @@
+import { z } from 'zod'
+
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  Param,
+  Put,
+} from '@nestjs/common'
+
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { Roles } from '@/infra/auth/authorization/roles'
+import { UserRoles } from '@/infra/auth/authorization/user-roles'
+import { UpdateRecipientProfileUseCase } from '@/domain/account/application/use-cases/update-recipient-profile'
+
+const updateRecipientProfileBodySchema = z.object({
+  name: z.string(),
+  cpf: z.string(),
+  street: z.string(),
+  number: z.number(),
+  neighborhood: z.string(),
+  city: z.string(),
+  cep: z.string(),
+  latitude: z.number(),
+  longitude: z.number(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(
+  updateRecipientProfileBodySchema,
+)
+
+type UpdateRecipientBodySchema = z.infer<
+  typeof updateRecipientProfileBodySchema
+>
+
+@Controller('/accounts/recipient/:id')
+@Roles(UserRoles.Admin)
+export class RegisterDeliverymanController {
+  constructor(private updateRecipientProfile: UpdateRecipientProfileUseCase) {}
+
+  @Put()
+  @HttpCode(204)
+  async handle(
+    @Body(bodyValidationPipe) body: UpdateRecipientBodySchema,
+    @Param('id') recipientId: string,
+  ) {
+    const {
+      name,
+      cpf,
+      street,
+      number,
+      neighborhood,
+      city,
+      cep,
+      latitude,
+      longitude,
+    } = body
+
+    const result = await this.updateRecipientProfile.execute({
+      recipientId,
+      name,
+      cpf,
+      street,
+      number,
+      neighborhood,
+      city,
+      cep,
+      latitude,
+      longitude,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
